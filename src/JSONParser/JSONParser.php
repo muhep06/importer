be5e15611c7f190e917ac3d3d84d57688964379d
Selennurdlsz <selennurdilsiz67@gmail.com>
2019-08-19 14:17:29 +0300

    Update JSONParser.php

--- src/JSONParser/JSONParser.php
+++ src/JSONParser/JSONParser.php
@@ -4,7 +4,31 @@
 namespace AcikVeri\Importer\JSONParser;
 
 
-class JSONParser
-{
+use AcikVeri\Importer\Interfaces\Importer;
+use GuzzleHttp\Client;
 
+
+class JSONParser implements Importer
+{
+    private $data;
+
+    /**
+     * @param $url
+     * @return $this
+     */
+    public function loadFromUrl($url)
+    {
+        $client = new Client();
+        $this->data = $client->get($url);
+        return $this;
+    }
+
+    /**
+     * @param $data
+     * @return $this
+     */
+    public function loadFromString($data) {
+        $this->data = simplejson_load_string($data);
+        return $this;
+    }
 }
