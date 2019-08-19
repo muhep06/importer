a632bc4255f1dc0e08ce51b1b7bfd7663a965b66
Muhep Atasoy <muhepatasoy06@gmail.com>
2019-08-19 14:10:34 +0300

    Update XMLParser.php

--- src/XMLParser/XMLParser.php
+++ src/XMLParser/XMLParser.php
@@ -4,7 +4,29 @@
 namespace AcikVeri\Importer\XMLParser;
 
 
+use GuzzleHttp\Client;
+
 class XMLParser
 {
+    private $data;
 
+    /**
+     * @param $url
+     * @return $this
+     */
+    public function loadFromUrl($url)
+    {
+        $client = new Client();
+        $this->data = $client->get($url);
+        return $this;
+    }
+
+    /**
+     * @param $data
+     * @return $this
+     */
+    public function loadFromString($data) {
+        $this->data = simplexml_load_string($data);
+        return $this;
+    }
 }
